02f5fef3227720e0d8add3dd85834a6b06415f82
DhitGT <adityadwisaputra666@gmail.com>
2024-06-02 20:48:57 +0700

    push

--- components/jumbotron.vue
+++ components/jumbotron.vue
@@ -49,7 +49,7 @@
             </svg>
           </a>
           <a
-            href="#"
+            href="#about-us"
             class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
           >
             Pelajari Lebih Lanjut
